35331ce3c09ce192e84e3d3c6b2e15d0e3aa6d52
futurefanzhang <zfan887@wsu.edu>
2020-03-28 15:58:11 -0700

    update function

--- gwas.R
+++ gwas.R
@@ -1,7 +1,7 @@
 #problem1
 AStest=function(y,X,C){ #y represent phenotype, X represent genotype, C represent covariates.
   y=y[,-1] #remove taxa name
-  C=C[,-1] #remove taxa name of covariates
+  C0=C[,-1] #remove taxa name of covariates
   G=X[,-1] #these code come from lecture of GLM
   n=nrow(G)
   m=ncol(G)
@@ -10,7 +10,7 @@ AStest=function(y,X,C){ #y represent phenotype, X represent genotype, C represen
     x=G[,i]
     if(max(x)==min(x)){
       p=1}else{
-        X1=cbind(1, C[,1:2],x)
+        X1=cbind(mean(y), C0, x)
         LHS=t(X1)%*%as.matrix(X1)
         C1=solve(LHS)
         RHS=t(X1)%*%y
@@ -29,41 +29,25 @@ AStest=function(y,X,C){ #y represent phenotype, X represent genotype, C represen
 }
 
 #problem2
-GWAS=function(y,X,C){ #y represent phenotype, X represent genotype, C represent covariates.
-  y=y[,-1] #remove taxa name
-  C=C[,-1] #remove taxa name of covariates
-  G=X[,-1]
+GWAS=function(y,X,C, pcas=3){
+  #y represent phenotype, X represent genotype, C represent covariates.
+  C0=C[,-1] #remove taxa name of covariates
+  G=X[,-1] #remove taxa name of genotype
+
   ###remove PC that are in linear dependent to the covariates
   PCA=prcomp(G)
-  r<-cor(PCA$x,C)
-  index1=r==1
+  r<-cor(PCA$x,C0)
+  index1=r > 0.9
   r[index1]=NA
-  r_remain=na.omit(r) #I don't know how to remove rows with value of 1, so I change 1 to NA, than remove them.
-  tPC=t(PCA$x)
-  keep_tPC<-tPC[rownames(tPC) %in% rownames(r_remain),]
-  keep_PC<-t(keep_tPC)
+  r_remain=na.omit(r)
+  keep_PC<-PCA$x[,colnames(PCA$x) %in% rownames(r_remain)]
+
   #GWAS related content
-  n=nrow(G)
-  m=ncol(G)
-  P=matrix(NA,1,m)
-  for (i in 1:m){
-    x=G[,i]
-    if(max(x)==min(x)){
-      p=1}else{
-        X1=cbind(1,keep_PC[,1:3],x) #only set PC1-3 as cofactor,right?
-        LHS=t(X1)%*%as.matrix(X1)
-        C1=solve(LHS)
-        RHS=as.matrix(t(X1))%*%y
-        b=C1%*%RHS
-        yb=as.matrix(X1)%*%b
-        e=y-yb
-        n=length(y)
-        ve=sum(e^2)/(n-1)
-        vt=C1*ve
-        t=b/sqrt(diag(vt))
-        p=2*(1-pt(abs(t),n-2))
-      } #end of testing variation
-    P[i]=p[length(p)]
-  } #end of looping for markers
+  if(pcas == FALSE){
+    C_all = C0
+  }else{
+    C_all = cbind(C0, keep_PC[,1:pcas])
+  }
+  P = AStest(y, X, C=C_all)
   return(P)
 }
